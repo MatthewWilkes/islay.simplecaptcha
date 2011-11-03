bb527f2e3ac10a8f0cdc5d3ce8c5bf10a7fd35a8
Matthew Wilkes <git@matthewwilkes.name>
2011-11-02 22:11:51 -0700

    First draft

--- src/islay/simplecaptcha/captcha.py
+++ src/islay/simplecaptcha/captcha.py
@@ -1,3 +1,5 @@
+from lxml import html
+from lxml.etree import XMLSyntaxError, Element
 from webob import Request, Response
 
 def CaptchaFactory(global_config, **local_conf):
@@ -10,6 +12,30 @@ class CaptchaMiddleware(object):
         self.app = app
     
     def __call__(self, environ, start_response):
+        # Before we go any further, gzip is hard to parse, don't ask for it
+        del environ['HTTP_ACCEPT_ENCODING']
+        
         request = Request(environ)
         response = request.get_response(self.app)
+        if response.content_type == 'text/html':
+            # We don't want to deal with images and the like
+            try:
+                parsed = html.fromstring(response.body)
+            except (XMLSyntaxError, TypeError):
+                return response(environ, start_response)
+            forms = parsed.xpath("//form")
+            index = 0
+            for form in forms:
+                box_id = "captcha%d" % index
+                checkbox = Element("input", type="checkbox", name="isHuman", value="1", id=box_id)
+                label = Element("label", attrib={'for':box_id})
+                label.text = "I am a human"
+                length = len(form.getchildren())
+                if length == 1:
+                    # Hack to add the captcha at the end of single element forms
+                    length = 2
+                form.insert(length - 1, checkbox) # The submit button is probably last
+                form.insert(length, label)
+            response.body = html.tostring(parsed)
         return response(environ, start_response)
+    
